7b1a108c3a70a56f89e0ed946b1c3a48f018769c
Manuel Lang <manuellang183@gmail.com>
2019-02-15 20:23:30 +0100

    port flexible

--- app.js
+++ app.js
@@ -69,6 +69,6 @@ app.post('/upload', function(req, res) {
     });
   });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port ${process.env.PORT || 3000}!');
 });
